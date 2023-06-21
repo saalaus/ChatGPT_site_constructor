c0f3369369b1662855868e5a6fb7304fa3b6dc6b
saalaus <saalaus2@gmail.com>
2023-06-21 20:39:17 +0500

    changes style, prompt

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_APIKEY")
 
 
-prompt = "Тебя зовут Пятница, ты личный ассистент для генерации HTML страниц с использованием CSS и JS." \
+prompt = "Ты стартуешь на пустой HTML странице где главный элемент div с id 'custom-content'." \
          "Пользователь вводит запрос, ты присылаешь данные JSON формата с готовым HTML код," \
          "без стандартной верстки типа html, head, body. Так же любому элементу который ты добавляешь добавляй" \
          " свой специальный класс, чтобы в последствии обращаться к элементу по классу." \
@@ -26,22 +26,9 @@ prompt = "Тебя зовут Пятница, ты личный ассистен
          "style - если это изменение или добавление стилей, стили нужно писать в ключе 'code', " \
          "никакие дополнительные поля от тебя не требуются и не пиши тег <style>. " \
          "Пиши все свои ответы в json формате," \
-         "если операций будет несколько, вставляй их внутри массива"
-
-# prompt = "Your name is Friday, you personal assistant for generating HTML pages using CSS and JS." \
-#          "The user enters a request, you send ready-made HTML code, without standard layout such as " \
-#          "html, head, body. You also add your own special class to any element that you add, " \
-#          "so that you can call it by its class. You don't address to body, " \
-#          "instead you have a special element with id 'custom-content', " \
-#          "address to it, and wrap all code in triple single quotes, " \
-#          "you should not print any additional information or explanations, only code. " \
-#          "Instead you should write type of operation: " \
-#          "add - add new element and write code of element, " \
-#          "style - if you change or add styles, styles should be written completely, without tag style " \
-#          "edit - change existing element, not styles, and the element to be edited, attribute to be" \
-#          " changed and new value and old value, " \
-#          "del - remove element and the element to be removed, " \
-#          "you should return the data in JSON format"
+         "если операций будет несколько, вставляй их внутри массива." \
+         "Пример: запрос от пользователя: сделай цвет страницы красным." \
+         "Твой ответ: {\"operation\": \"style\", \"code\": \"#custom-content{background-color: red}\"}"
 
 
 start_context = [dict(role="system", content=prompt)]
--- static/index.js
+++ static/index.js
@@ -15,39 +15,6 @@ function makeData(data){
     }
 }
 function sendRequest(message){
-  //   const xhttp = new XMLHttpRequest();
-  // xhttp.onload = function() {
-  //     const add_style = document.getElementById("custom-style")
-  //     const add_element = document.getElementById("custom-content")
-  //     const text = JSON.parse(this.responseText)
-  //     console.log(text)
-  //     if (text.action === "new_style"){
-  //         console.log("new_style")
-  //         add_style.innerHTML += text.data
-  //     }
-  //     if (text.action === "add"){
-  //         console.log("new_element")
-  //         add_element.insertAdjacentHTML("beforeend", text.data)
-  //     }
-  //     context.push({role: "assistant", content: text.message})
-      // const text = this.responseText.split(/\r?\n/)
-      // const last_line = text.pop()
-      // console.log(text)
-      // console.log(last_line)
-      // if (text.join().startsWith('```html')) {
-      //     console.log()
-          // chat_text_el.innerHTML += '<p class="text-left">' + last_line + '</p>'
-          // const text2 = text.join('\n').slice(7, text.join('\n').length-3)
-          // document.body.insertAdjacentHTML('beforeend', text2)
-      // }
-      // else{
-      //     chat_text_el.innerHTML += '<p class="text-left">' + text + '</p>'
-      // }
-
-    // }
-    // context.push({role: "user", content: message})
-    // xhttp.open('POST', '/chatgpt', true)
-    // xhttp.send(JSON.stringify(context))
     context.push({role: "user", content: message})
 
     return fetch("/chatgpt", {
@@ -61,16 +28,13 @@ function sendRequest(message){
         })
 }
 
-const chat_text_el = document.getElementById('chat-text-block')
-chat_text_el.scrollTo(0, chat_text_el.scrollHeight)
+
 const text_input = document.getElementById('text-input');
 text_input.addEventListener("keydown", (event) => {
     if (event.code === 'Enter'){
         const text = text_input.value
-        chat_text_el.innerHTML += '<p class="text-right">' + text_input.value + '</p>'
-        text_input.value = '';
-        chat_text_el.scrollTo(0, chat_text_el.scrollHeight)
         sendRequest(text).then(data => makeData(data))
+        text_input.value = ""
         return false;
     }
 })

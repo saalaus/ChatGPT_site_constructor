46c827a859b84ea9e686581d3558d827b669a21a
saalaus <saalaus2@gmail.com>
2024-03-09 15:44:43 +0500

    add model to .env | update open ai | refactoring | add split.js

--- app.py
+++ app.py
@@ -2,94 +2,64 @@ import json
 import os
 
 import openai
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_APIKEY")
+client = openai.OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+)
 
 
-prompt = "Ты стартуешь на пустой HTML странице где главный элемент div с id 'custom-content'." \
-         "Пользователь вводит запрос, ты присылаешь данные JSON формата с готовым HTML код," \
-         "без стандартной верстки типа html, head, body. Так же любому элементу который ты добавляешь добавляй" \
-         " свой специальный класс, чтобы в последствии обращаться к элементу по классу." \
-         " Не обращайся к body, вместо этого есть специальный элемент с айди 'custom-content' - это вся страница" \
-         ", обращайся к нему если" \
-         "нужно изменить стили страницы, так же не нужно никаких объяснений и " \
-         "дополнительной информации, вместо этого пиши тип операции в ключе 'operation': " \
-         "add - добавление нового элемента и напиши код этого элемента," \
-         "edit - изменение существующего элемента, и сам элемент который нужно редактировать, атрибут который надо" \
-         " изменить, и на что его надо поменять, " \
-         "del - удаление элемента и сам элемент который нужно удалить, " \
-         "style - если это изменение или добавление стилей, стили нужно писать в ключе 'code', " \
-         "никакие дополнительные поля от тебя не требуются и не пиши тег <style>. " \
-         "Пиши все свои ответы в json формате," \
-         "если операций будет несколько, вставляй их внутри массива." \
-         "Пример: запрос от пользователя: сделай цвет страницы красным." \
-         "Твой ответ: {\"operation\": \"style\", \"code\": \"#custom-content{background-color: red}\"}"
+prompt = """
+You start on a blank HTML page with a main div element with the id "custom-content".
+The user enters a request, you send ready HTML code, without standard layout like html, head, body. To the added elements add your class or id or any other identifier and create containers around the elements if you think necessary.
+Don't refer to body, instead have an element with id "custom-content" which is the whole page.
+In response, I expect a JSON object of the form {"operation": TYPE_OPERATION} from you, with additional fields depending on the operation
+Possible TYPE_OPERATIONS:
+add - adding a new HTML element {"operation": "add", "code": ADD_Element}, also you can use the "to" OR "before" OR "after" key to explicitly specify where to add the element as a child.
+edit - modify an element {"operation": "edit", "element": QUERY_SELECTOR_ELEMENT, "attribute": CHANGEABLE_ATTRIBUTE, "value": CHANGEABLE ATTRIBUTE VALUE}
+del - delete element { "operation": "del", "element": QUERYSELECTOR_DELETE_ELEMENT}
+code - adding ONLY JavaScript code {"operation": "code", "code": CODE}
+style - changing or adding styles {"operation": "style", "code": CODE}
+list - if you need to perform several operations at once {"operation": "list", "code": [OPERATIONS DESCRIBED ABOVE]}
+"""
 
-
-start_context = [dict(role="system", content=prompt)]
+start_context = [{"role": "system", "content": prompt}]
 
 app = Flask(__name__)
 
 
-def get_code_from_completion(message: str) -> str:
-    if "```" in message:
-        return message.split("```")[1]
-    elif "'''" in message:
-        return message.split("'''")[1]
-    elif '"""' in message:
-        return message.split('"""')[1]
-    elif len(message.split("-")) > 1:
-        return message.split("-")[1]
-
-
-def format_completion(message: str) -> dict:
-    message_lines = message.splitlines()
-    return_data = dict(message=message)
-    if message.startswith("style"):
-        return_data["action"] = "new_style"
-        return_data["data"] = get_code_from_completion(message)
-    if message.startswith("add"):
-        return_data["action"] = "add"
-        return_data["data"] = get_code_from_completion(message)
-
-    return return_data
-
-
-def get_completion(context: list) -> str:
-    # messages.append({"role": "user", "content": message})
-    # current_message = messages + [{"role": "user", "content": message}]
-    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=start_context + context, temperature=0)
+def get_completion(context):
+    completion = client.chat.completions.create(
+        model=os.getenv("MODEL", "gpt-3.5-turbo"),
+        messages=(start_context + context),
+        temperature=0,
+        response_format={"type": "json_object"},
+    )
 
     chat_response = completion.choices[0].message.content
-
-    # messages.append({"role": "assistant", "content": chat_response})
 
     return chat_response
 
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html")
 
 
-@app.route("/chatgpt", methods=["POST"])
-def send_message():
+@app.route("/completion", methods=["POST"])
+def handle_chat_request():
     context = json.loads(request.data)
 
     chat_response = get_completion(context)
 
     print(chat_response)
 
-    return chat_response
+    return jsonify({"result": json.loads(chat_response)}), 200
 
 
 if __name__ == "__main__":
     app.run()
-    # while True:
-    #     message = input("> ")
-    #     print(get_completion(message))
--- static/index.js
+++ static/index.js
@@ -2,38 +2,90 @@ let context = []
 const add_style = document.getElementById("custom-style")
 const add_element = document.getElementById("custom-content")
 
+
+Split(["#split-0", "#split-1", "#split-2"], {
+    minSize: 0,
+    sizes: [90, 10, 10],
+});
+
+
+function replaceScript(addCode){
+    const add_code = document.getElementById("custom-code");
+    const value = add_code.innerText
+
+    var newScriptTag = document.createElement("script");
+    newScriptTag.id = "custom-code"
+    newScriptTag.textContent = value + addCode + "\n"
+    add_code.parentNode.replaceChild(newScriptTag, add_code)
+}
+
+function addElement({code, to, before, after}){
+    const add_element = document.querySelector(to) ||
+                        document.getElementById("custom-content");
+
+    if(before){
+        add_element.insertAdjacentHTML("beforebegin", code)
+    }
+    else if(after){
+        add_element.insertAdjacentHTML("afterend", code)
+    }
+    else{
+        add_element.insertAdjacentHTML("beforeend", code)
+    }
+}
+
 function makeData(data){
     switch (data.operation) {
         case "style":
-            add_style.innerHTML += data.code
+            add_style.innerHTML += data.code + '\n'
             break
         case "add":
-            add_element.insertAdjacentHTML("beforeend", data.code)
+            addElement(data)
+            break
+        case "code":
+            replaceScript(data.code);
+            break
+        case "edit":
+            document.querySelector(data.element)[data.attribute] = data.value
+            break
+        case "del":
+            document.querySelector(data.element).remove()
+            break
+        case "list":
+            data.code.forEach(element => {
+                makeData(element)
+            });
             break
         default:
             console.error(data)
+            break
     }
 }
 function sendRequest(message){
     context.push({role: "user", content: message})
 
-    return fetch("/chatgpt", {
+    return fetch("/completion", {
         method: "POST",
-        body: JSON.stringify(context)
+        body: JSON.stringify(context),
     })
-        .then(data => data.json())
-        .then(data => {
-            context.push({role: "assistant", content: JSON.stringify(data)})
-            return data
-        })
+        .then((data) => data.json())
+        .then((data) => data.result)
+        .then((data) => {
+            context.push({ role: "assistant", content: JSON.stringify(data) });
+            return data;
+        });
 }
 
 
 const text_input = document.getElementById('text-input');
 text_input.addEventListener("keydown", (event) => {
     if (event.code === 'Enter'){
+        text_input.disabled = true
         const text = text_input.value
-        sendRequest(text).then(data => makeData(data))
+        sendRequest(text).then(data => {
+            makeData(data)
+            text_input.disabled = false;
+        })
         text_input.value = ""
         return false;
     }
